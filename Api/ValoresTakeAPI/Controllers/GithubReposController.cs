fdde72d692497ca05a28577bf329f969e7cecf2e
thalesken <thales_ken@hotmail.com>
2024-06-13 00:27:23 -0300

    feat: add controller

---
+++ Api/ValoresTakeAPI/Controllers/GithubReposController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValoresTakeAPI.Controllers;
+
+public class GithubReposController : ControllerBase
+{
+    private readonly IGithubService _githubService;
+
+    public GithubReposController(IGithubService githubService)
+    {
+        _githubService = githubService;
+    }
+
+    [HttpGet("five-oldest-csharp-repos")]
+    public async Task<IActionResult> GetFiveOldestCsharpRepos()
+    {
+        var response = await _githubService.GetFiveOldestCsharpReposAsync();
+        if (response.Count == 0)
+        {
+            return NotFound();
+        }
+        return Ok(response);
+    }
+}
--- Api/ValoresTakeAPI/Program.cs
+++ Api/ValoresTakeAPI/Program.cs
@@ -1,4 +1,6 @@
-using Domain.Extensions;
+using Application.Services;
+using Domain.Interfaces;
+using Domain.Settings;
 using Infrastructure.Api;
 using RestEase;
 
@@ -10,7 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddClient();
+builder.Services.AddSingleton(RestClient.For<IGithubClient>("https://api.github.com"));
+builder.Services.AddScoped<IGithubService, GithubService>();
+var settings = builder.Configuration.GetSection("Settings").Get<CustomSettings>();
+#pragma warning disable CS8634 // O tipo não pode ser usado como parâmetro de tipo no tipo ou método genérico. A anulabilidade do argumento de tipo não corresponde à restrição 'class'.
+builder.Services.AddSingleton(settings);
+#pragma warning restore CS8634 // O tipo não pode ser usado como parâmetro de tipo no tipo ou método genérico. A anulabilidade do argumento de tipo não corresponde à restrição 'class'.
 
 var app = builder.Build();
 
