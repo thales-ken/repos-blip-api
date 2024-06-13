0010a96336a6a693f03d587c2ff9668ccf3dd313
thalesken <thales_ken@hotmail.com>
2024-06-12 21:34:06 -0300

    feat: create github response

---
+++ Api/Domain/Entities/GithubResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
+
+public class GithubResponse
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("owner")]
+    public Owner? Owner { get; set; }
+}
---
+++ Api/Domain/Entities/Owner.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
+
+public class Owner
+{
+    [JsonPropertyName("avatar_url")]
+    public string? AvatarUrl { get; set; }
+}
